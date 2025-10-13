98847f80677d83be45c07c66b8e8ca9178340d28
Fernando Romero Barrigas <146009301+9Fernando@users.noreply.github.com>
2025-10-13 20:42:23 +0200

    Test1
    Primer test para probar el motor y las capacidades a nivel básico

---
+++ TEST1/Assets/UINameToText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.N3DS;
+
+public class UINameToText : MonoBehaviour {
+
+	private Text t;
+	void Start() {
+		t = GetComponent<Text>();
+		string User;
+		bool isProfane;
+		UnityEngine.N3DS.Config.GetUserName(out User, out isProfane);
+		t.text = User;
+	}
+	
+}
